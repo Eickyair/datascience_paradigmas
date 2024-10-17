7eeca880d96f9ec4fab561631ded4ffce6d9d8e9
Eickyair <martinezerickaguilar@comunidad.unam.mx>
2024-10-17 10:41:20 -0600

    Agregar función para validar y copiar archivos con formato específico

--- datasets/covid/escala_grises/entrenamientos/union_datos.sh
+++ datasets/covid/escala_grises/entrenamientos/union_datos.sh
@@ -4,6 +4,44 @@
 output_path=""
 source_path=""
 
+validar_archivo() {
+    local ruta_archivo_origen=$1
+    local ruta_archivo_destino=$2
+    local lineas_validas=0
+    local archivo_temp=$(mktemp)
+
+    while IFS= read -r linea; do
+        # Validar el formato de la línea
+        if ! [[ $linea =~ ^0\ [0-9]+\.[0-9]{6}\ [0-9]+\.[0-9]{6}\ [0-9]+\.[0-9]{6}\ [0-9]+\.[0-9]{6}$ ]]; then
+            # Validar que el primer elemento sea 0
+            primer_elemento=$(echo $linea | awk '{print $1}')
+            if [ "$primer_elemento" -ne 0 ]; then
+                echo "Correccion 0: $linea"
+                linea="0 $(echo $linea | cut -d' ' -f2-)"
+                ((lineas_validas++))
+            fi
+        else
+            ((lineas_validas++))
+        fi
+        echo "$linea" >> "$archivo_temp"
+    done < "$ruta_archivo_origen"
+
+    # Mostrar mensaje si el archivo tiene más de una línea válida y parar la ejecución
+    if [ "$lineas_validas" -gt 1 ]; then
+        echo "El archivo $ruta_archivo_origen tiene más de una línea válida."
+        rm "$archivo_temp"
+        return 1
+    fi
+
+    # Si hay una única línea válida, copiar el archivo temporal al destino
+    if [ "$lineas_validas" -eq 1 ]; then
+        mv "$archivo_temp" "$ruta_archivo_destino"
+        echo "El archivo ha sido copiado a $ruta_archivo_destino."
+    else
+        echo "No se encontraron líneas válidas en el archivo."
+        rm "$archivo_temp"
+    fi
+}
 # Función para obtener nombres de archivos sin extensiones
 obtener_nombres_sin_extension() {
     local carpeta=$1
@@ -12,15 +50,19 @@ obtener_nombres_sin_extension() {
 
 # Comparar archivos entre dos carpetas
 comparar_archivos() {
-    local carpeta1=$1
-    local carpeta2=$2
-    nombres1=$(obtener_nombres_sin_extension "$carpeta1")
-    nombres2=$(obtener_nombres_sin_extension "$carpeta2")
-
+    local carpeta_images=$1
+    local carpeta_etiquetas=$2
+    nombres1=$(obtener_nombres_sin_extension "$carpeta_images")
+    nombres2=$(obtener_nombres_sin_extension "$carpeta_etiquetas")
+    mkdir -p $output_path/etiquetas
     for nombre in $nombres1; do
         if ! echo "$nombres2" | grep -q "^$nombre$"; then
             echo "El archivo $nombre no está en $carpeta2"
+            continue
         fi
+        local ruta_archivo="$carpeta_etiquetas/$nombre.txt"
+        echo $ruta_archivo
+        validar_archivo "$ruta_archivo" "$output_path/etiquetas/$nombre.txt"
     done
 }
 
